b5aeb405c0b63c35fa9064854c0ac6fd6736ff4a
OGI_codes <ognjen495@gmail.com>
2020-04-15 18:30:07 +0200

    Slider functionality done

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,11 @@
 const baseUrl = "https://api.themoviedb.org/3/";
 const apiKey = "3bd5a7563d2f1a399fa2c575c72eb467";
 const latestMovies = document.querySelectorAll(".movie-container")
+
+latestMovies.forEach(e => {
+    e.lastElementChild.lastElementChild.addEventListener('click', modalWindowActivator);
+})
+
 var config;
 function getConfig(){
     var url = baseUrl+"configuration?api_key="+apiKey;
@@ -11,14 +16,14 @@ function getConfig(){
 }
 getConfig();
 
-var a;
+/*var a;
 function lastMovies(){
     var url = baseUrl + "movie/latest?api_key="+apiKey;
     fetch(url).then(e => e.json()).then(e => {
         a=e
         const slika = document.getElementById("slika").src = config.images.secure_base_url+config.images.poster_sizes[2]+a.poster_path;
     });
-}
+}*/
 
 var arrayOfNows;
 function nowPlaying(){
@@ -32,6 +37,7 @@ function nowPlaying(){
         imageSliderTracker(9);
     })
 }
+
 function imageSliderTracker(x){
     for(var i = 0;i < 5;i++){
         let e = latestMovies[i];
@@ -42,6 +48,8 @@ function imageSliderTracker(x){
         x++
     }
 }
+
+
 var imageTracker = 9;
 function imageSliderLeft(){
     document.querySelector(".latest-movies-container").style.animation = "imageSliderLeft 0.5s"
@@ -52,12 +60,41 @@ function imageSliderLeft(){
         imageSliderTracker(imageTracker);
     },500);
 }
+
 function imageSliderRight(){
     document.querySelector(".latest-movies-container").style.animation = "imageSliderRight 0.5s"
     setTimeout(function(){
-        document.querySelector(".latest-movies-container").style.animation = "";
+        document.querySelector(".latest-movies-container").style.animation = "";            
         imageTracker++;
         if(imageTracker > 9) imageTracker = imageTracker - 10;
         imageSliderTracker(imageTracker);
     },500);
+}
+
+const imageWindow = document.querySelector("#window");
+var windowFlag = false;
+imageWindow.addEventListener('mouseover', function(){windowFlag = true});
+imageWindow.addEventListener('mouseleave', function(){windowFlag = false});
+window.addEventListener('keydown', keyImageTracker);
+
+setInterval(function(){
+    if (!windowFlag) {
+        imageSliderRight();
+    }
+},3000)
+
+function keyImageTracker(e){
+    if (windowFlag) {
+        if(e.keyCode == 39){
+            imageSliderRight();
+        } else if(e.keyCode == 37){
+            imageSliderLeft();
+        }
+    }
+}
+
+const modalWindow = document.querySelector("#modal-window")
+
+function modalWindowActivator(e){
+    modalWindow.classList.toggle("active-modal-window");
 }
